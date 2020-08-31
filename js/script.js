9d2cdb709f503c3ac2637cd0e012088cfce74b5f
João Barcelos Coutinho <jabcneto@gmail.com>
2020-08-31 16:30:36 -0300

    refatorando script

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
-document.querySelectorAll("a").forEach((link) => {
-  const conteudo = document.getElementById("conteudo");
-  link.onclick = function (e) {
+document.querySelectorAll("[urlAjax]").forEach((link) => {
+  const urlAjax = link.attributes["urlAjax"].value;
+  const destino = link.attributes["destino"].value;
+  console.log("klaisdnasdkjn");
+  link.onclick = (e) => {
     e.preventDefault();
-    conteudo.innerHTML = "";
 
-    fetch(link.href)
-      .then((resp) => resp.text())
-      .then((html) => (conteudo.innerHTML = html))
-      .catch(e => console.log(e));
+    navegacao(urlAjax, destino);
   };
 });
 
+function navegacao(url, seletor) {
+  if (!url || !seletor) return;
+
+  const elemento = document.querySelector(seletor);
+  fetch(url)
+    .then((res) => res.text())
+    .then((html) => {
+      elemento.innerHTML = html;
+    });
+}
+
+function login() {
+  document.getElementById("nomeLogado").innerHTML = "Bem vindo fulano";
+}
