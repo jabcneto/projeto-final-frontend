02e0ee4c17f95983de2a79cdb9016baabd20c9ba
João Barcelos Coutinho <jabcneto@gmail.com>
2020-09-01 09:10:13 -0300

    esquece o fake react

--- js/script.js
+++ js/script.js
@@ -1,20 +1,4 @@
-document.querySelectorAll("[urlAjax]").forEach((link) => {
-  const urlAjax = link.attributes["urlAjax"].value;
-  const destino = link.attributes["destino"].value;
-  link.onclick = (e) => {
-    e.preventDefault();
 
-    navegacao(urlAjax, destino);
-  };
-});
 
-function navegacao(url, seletor) {
-  if (!url || !seletor) return;
 
-  const elemento = document.querySelector(seletor);
-  fetch(url)
-    .then((res) => res.text())
-    .then((html) => {
-      elemento.innerHTML = html;
-    });
-}
+
