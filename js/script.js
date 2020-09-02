ea646577aaebf1c55367064e579e0ad2544c773e
João Barcelos Coutinho <jabcneto@gmail.com>
2020-09-02 12:48:24 -0300

    Adicionando "login com social" / resolvendo null

--- js/script.js
+++ js/script.js
@@ -16,17 +16,46 @@ const filtraCategoria = (categoria) => {
   });
 };
 
+// LOGIN / AUTENTICACAO / LOGOUT
+myStorage = window.sessionStorage;
+
 (function verificaLogin() {
-  console.log('Verificando Login');
-  myStorage = window.sessionStorage;
+  console.log("Verificando Login");
   let email = myStorage.getItem("email");
   let senha = myStorage.getItem("senha");
   let nome = myStorage.getItem("nome");
 
+  $("#botao-entrar").show();
+  $("#usuario-logado").hide();
+  $("#botao-sair").hide();
 
-  if (email !== undefined && senha !== undefined) {
-    $('#botao-entrar').toggle()
+  if (email !== null && senha !== null) {
+    $("#botao-entrar").hide();
+    $("#usuario-logado").show();
+    $("#botao-sair").show();
     console.log(`Usuario Logado: ` + nome);
     document.querySelector("#usuario-logado").innerText = `Bem vindo, ${nome}`;
   }
 })();
+
+const login = () => {
+  let email = document.querySelector("#inputEmail").value;
+  let senha = document.querySelector("#inputSenha").value;
+
+  if (email === "admin@admin" && senha === "admin") {
+    myStorage.setItem("email", email);
+    myStorage.setItem("senha", senha);
+    myStorage.setItem("nome", "admin");
+  }
+};
+
+const loginSocial = () => {
+  myStorage.setItem("email", email);
+  myStorage.setItem("senha", senha);
+  myStorage.setItem("nome", "admin");
+};
+
+const logout = () => {
+  myStorage.clear();
+  location.reload();
+};
